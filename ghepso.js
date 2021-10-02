c45ccd14137046abf359665f9cd8c88b94d3061b
TuanzNguyez <you@example.com>
2021-10-02 16:22:05 +0700

    okk

--- ghepso.js
+++ ghepso.js
@@ -1,3 +1,7 @@
+audioExactly = new Audio('./mp3/a.mp3');
+audioPass = new Audio('./mp3/b.mp3');
+audioWin = new Audio('./mp3/c.mp3')
+
 const loadHtml = () => {
     const url = "https://6152ead9c465200017d1a870.mockapi.io/ghepSo";
     var levelElement = document.querySelector('#selectLevel');
@@ -72,57 +76,63 @@ const handlePlaying = (url, minTimeElement, level,levelElement) => {
     // xử lý sự kiện khi click vào item 
     const handleSelectItem = (e) => {
         var attr = e.target.attributes;
+        var ngat;
         
         if (value == attr.value.nodeValue && index != attr.index.nodeValue) {
             // khi hai element có cùng value
-            if (firstElement) {
-                // hiệu ứng khi click
-                e.target.parentElement.style.transform = 'rotateX(0deg)';
-                e.target.innerHTML = attr.value.nodeValue;
+            let newE = e.target;
+            let newF = firstElement;
 
-                firstElement.parentElement.style.transform = 'rotateX(0deg)';
-                firstElement.innerHTML = attr.value.nodeValue;
+            // hiệu ứng khi click
+            clearTimeout(ngat);
+            newE.parentElement.style.transform = 'rotateX(0deg)';
+            newE.innerHTML = attr.value.nodeValue;
 
-                //ẩn đi element khi click vào hai element có cùng value
-                setTimeout(() => {
-                    e.target.parentElement.style.width = '0px';
-                    e.target.parentElement.style.margin = '0px';
-                    e.target.parentElement.style.padding = '0px';
-                    firstElement.parentElement.style.width = '0px';
-                    firstElement.parentElement.style.margin = '0px';
-                    firstElement.parentElement.style.padding = '0px';
+            newF.parentElement.style.transform = 'rotateX(0deg)';
+            newF.innerHTML = attr.value.nodeValue;
+            
+            
+            //ẩn đi element khi click vào hai element có cùng value
+            setTimeout(() => {
+                // âm thanh
+                audioExactly.play();
 
-                    firstElement = e.target;
-                    value = attr.value.nodeValue;
-                    index = attr.index.nodeValue;
+                newE.parentElement.style.width = '0px';
+                newE.parentElement.style.margin = '0px';
+                newE.parentElement.style.padding = '0px';
+                newF.parentElement.style.width = '0px';
+                newF.parentElement.style.margin = '0px';
+                newF.parentElement.style.padding = '0px';
+            }, 1000);
 
-                }, 500);
-                dem++;
-                if (dem == level * 12 / 2) {
-                    let text = document.querySelectorAll('.scores span');
-                    let name = document.querySelector('.player input').value;
-                    name = name ? name : 'Ẩn Danh';
+            dem++;
+            // qua màn
+            if (dem == level * 12 / 2) {
+                let text = document.querySelectorAll('.scores span');
+                let name = document.querySelector('.player input').value;
+                name = name ? name : 'Ẩn Danh';
 
-                    let h = text[0].innerHTML
-                    let m = text[1].innerHTML
-                    let s = text[2].innerHTML
-                    let time = s * 1 + m * 60 + h * 60 * 60;
-                    let data = {
-                        name: name,
-                        time: time,
-                        level: level,
-                    }
-                    pulPlayer(url, data);
-                    winGame(url,minTimeElement, level,levelElement);
-                    clearInterval(lap);
+                let h = text[0].innerHTML
+                let m = text[1].innerHTML
+                let s = text[2].innerHTML
+                let time = s * 1 + m * 60 + h * 60 * 60;
+                let data = {
+                    name: name,
+                    time: time,
+                    level: level,
                 }
+                pulPlayer(url, data);
+                winGame(url,minTimeElement, level,levelElement);
+                clearInterval(lap);
+                if (level==10) audioWin.play()
+                else audioPass.play();
             }
 
         } else {
             // khi click vào item
             e.target.parentElement.style.transform = 'rotateX(0deg)';
             e.target.innerHTML = attr.value.nodeValue;
-            setTimeout(() => {
+            ngat = setTimeout(() => {
                 e.target.parentElement.style.transform = 'rotateX(360deg)';
                 e.target.innerHTML = '?'
             }, 1500);
@@ -227,17 +237,16 @@ const pulPlayer = (url, dt) => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(dt),
     })
-        .then(response => response.json())
-        .then(data => {
-            console.log('success', data)
-        })
+    .then(response => response.json())
+    .then(data => {
+        console.log('success', data)
+    })
 }
 // xử lý khi chơi xong 1 level
 const winGame = (url,minTimeElement, level,levelElement) => {
     var contentElement = document.querySelector('.content .row');
     const nextLevel = () => {
         level=level*1;
-        console.log(levelElement)
         levelElement.value=level+1
         selectLevel(level+1);
         handlePlaying(url, level+1);
